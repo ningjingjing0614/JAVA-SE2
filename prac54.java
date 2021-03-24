aa087109c18a4d2a05cc4e0efd7ca5bb02abc960
ningjngjing0614 <215937309@qq.com>
2021-03-24 20:58:42 +0800

    宁静静：test

---
+++ prac54.java
@@ -0,0 +1,62 @@
+public class prac54 {
+////    写一个递归方法，输入一个非负整数，返回组成它的数字之和
+//public static int numAdd(int num) {
+//    if(num / 10 == 0){
+//        return (num / 10) + (num % 10);
+//    }
+//    return numAdd(num % 10) + numAdd(num / 10);
+//}
+//
+//    public static void main(String[] args) {
+//        System.out.println(numAdd(123422));
+//    }
+//}
+
+//class Base{
+//
+//    public Base(String s){
+//
+//        System.out.print("B");
+//
+//    }
+//
+//}
+//
+//public class Derived extends Base{
+//
+//    public Derived (String s) {
+////super("d");
+//        System.out.print("D");
+//
+//    }
+//
+//    public static void main(String[] args){
+//
+//        new Derived("C");
+//
+//    }
+//}
+
+//class Base {
+//
+//    Base() {
+//
+//        System.out.print("Base");
+//
+//    }
+//
+//}
+//
+//public class Alpha extends Base {
+//
+//    public static void main( String[] args ) {
+//
+//        new Alpha();
+//
+//        //调用父类无参的构造方法
+//
+//        new Base();
+//
+//    }
+//
+}
